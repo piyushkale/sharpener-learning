2305f4d1a180c37eee62ccd1879fb72af9c69fe9
piyushkale <97799152+piyushkale@users.noreply.github.com>
2026-01-12 12:25:40 +0530

    Nodemon,package.json and npm -  Sharpener

---
+++ npmLearning.js
@@ -0,0 +1,21 @@
+// Nodemon,package.json and npm
+
+/*
+Nodemon- is it a npm package, it used to automatically restart node server upon making any change to the code.
+        installing nodemon cmd- npm i nodemon --save-dev 
+
+Package json - This file contains metadata about the project and manage depedencies.
+ It includes information such as the project name, version, dependencies, scripts, and more.
+        Project is initialized by creating this file - npm init
+
+npm - npm is the default package manager for Node.js, allowing you to install, share, and manage dependencies in your projects.
+installing npm package - npm install <package-name>
+unistalling package - npm uninstall <package-name>
+
+Dev dependencies - Dependencies useful/required only during development of Project.
+ex- nodemon
+
+Normal dependencies - Functionality of project depends on these dependecies.
+ex - express
+
+*/ 
