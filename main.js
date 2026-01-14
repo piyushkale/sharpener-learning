deb2122562bc10197024a04584c94396b2cf1445
piyushkale <97799152+piyushkale@users.noreply.github.com>
2026-01-14 18:19:15 +0530

    Unknown route
    Build an Express server that listens on port 4000 and handles the following endpoints:
    GET /products - Respond with "Here is the list of all products."
    POST /products - Respond with "A new product has been added."
    GET /categories - Respond with "Here is the list of all categories."
    POST /categories - Respond with "A new category has been created."
    Wildcard Route

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-const express = require('express');
-const app = express();
-const port = 3000;    
 
-app.use('/welcome',(req,res,next)=>{
-    req.user = "Guest";
-    console.log("Middleware executed: User set to Guest");
-    next()
-});
+const express = require('express')
+const app = express()
 
-app.get('/welcome', (req, res) => {
-  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+app.get('/',(req,res)=>{
+    res.send('Home')
+})
+app.get('/products',(req,res)=>{
+    res.send('Here is the list of all products.')
 })
 
-app.listen(port,()=>{
-    console.log(`Server is up and running on port ${port}! Ready to handle requests.`)
+app.post('/products',(req,res)=>{
+    res.send('A new product has been has added.')
 })
+
+app.get('/categories',(req,res)=>{
+    res.send('Here is the list of all categories')
+})
+
+app.post('/categories',(req,res)=>{
+    res.send('A new category has been has added')
+})
+
+app.use((req,res)=>{
+    res.status(404).send(`<h1>404 - Page Not Found</h1>`)
+})
+
+
+app.listen(4000)
