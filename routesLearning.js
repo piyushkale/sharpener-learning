bcde4c230e15f1da132569c31bfa0aeca86ff4b7
piyushkale <97799152+piyushkale@users.noreply.github.com>
2026-01-14 14:50:47 +0530

    Question-Routes
    : Express Server and it's different routes !

---
+++ routesLearning.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;  
+
+app.get('/orders',(req,res)=>{
+    res.send('Here is the list of all orders.')
+})
+
+app.post('/orders',(req,res)=>{
+    res.send('A new order has been created.')
+})
+
+app.get('/users',(req,res)=>{
+    res.send('Here is the list of all users.')
+})
+
+app.post('/users',(req,res)=>{
+    res.send('A new user has been added.')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
